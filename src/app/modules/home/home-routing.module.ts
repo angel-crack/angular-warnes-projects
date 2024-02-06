bfa34ef7af3f943f78ddea50f93624eaac129f85
AngelCrack <angel.ed98@outlook.com>
2024-02-06 13:11:59 -0500

    export sidebar from shared

--- src/app/modules/home/home-routing.module.ts
+++ src/app/modules/home/home-routing.module.ts
@@ -5,7 +5,7 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
 
 const routes: Routes = [
   {
-    path: 'dashboard',
+    path: '',
     component: HomePageComponent
   }
 ];
