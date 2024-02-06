52db09e3cd2cf545a29645bbd0731443faf94c6a
AngelCrack <angel.ed98@outlook.com>
2024-02-05 19:22:26 -0500

    config ts

---
+++ src/app/modules/home/home-routing.module.ts
@@ -0,0 +1,17 @@
+import { Component, NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+
+
+const routes: Routes = [
+  {
+    path: 'dashboard',
+    component: HomePageComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class HomeRoutingModule { }
