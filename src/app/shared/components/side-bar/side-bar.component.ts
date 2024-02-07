5d9d5797e7adcfad7a96b23dba290ac18a884f23
AngelCrack <angel.ed98@outlook.com>
2024-02-07 12:22:26 -0500

    pages added to his module

--- src/app/shared/components/side-bar/side-bar.component.ts
+++ src/app/shared/components/side-bar/side-bar.component.ts
@@ -6,5 +6,62 @@ import { Component } from '@angular/core';
   styleUrls: ['./side-bar.component.css']
 })
 export class SideBarComponent {
+  mainMenu: {defaultOptions:Array<any>, accessLink:Array<any>} = {
+    defaultOptions: [
+      {
+        name: 'Home',
+        icon: 'uil uil-estate',
+        router: ['/']
+      },
+      {
+        name: 'Buscar',
+        icon: 'uil uil-search',
+        router: ['/', 'history']
+      },
+      {
+        name: 'Tu Biblioteca',
+        icon: 'uil uil-chart',
+        router: ['/', 'favorites']
+      }
+    ],
+    accessLink: [
+      {
+        name: 'Crear Lista',
+        icon: 'uil-plus-square'
+      },
+      {
+        name: 'Canciones que te gustan',
+        icon: 'uil-heart-medical'
+      }
+    ]
+  }
+  customOptions:Array<any> = [
+    {
+      name: 'Mi Lista °1',
+      router: ['/']
+    },
+    {
+      name: 'Mi Lista °2',
+      router: ['/']
+    },
+    {
+      name: 'Mi Lista °3',
+      router: ['/']
+    },
+    {
+      name: 'Mi Lista °4',
+      router: ['/']
+    }
 
+  ]
+  linksMenu: Array<any> = [
+    {
+      name: 'Home',
+      icon: "uil uil-estate"
+    },
+    {
+      name: 'Buscar',
+      icon: "uil uil-estate"
+    }
+  ];
 }
