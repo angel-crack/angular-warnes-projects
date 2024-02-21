c18ca09a7c64b0448e98d4c1157d0d4da14a585a
AngelCrack <prueba@Mac-mini-de-Prueba.local>
2024-02-21 10:01:30 -0500

    ng-directives added

--- src/app/shared/directives/img-broken.directive.ts
+++ src/app/shared/directives/img-broken.directive.ts
@@ -1,15 +1,17 @@
-import { Directive, ElementRef, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: 'img[appImgBroken]'
 })
 export class ImgBrokenDirective {
+  @Input() customImg: string = '../../../assets/images/imgError.png'
+
   @HostListener('error') handleError(): void{
      const elNative = this.elHost.nativeElement
-     console.log(elNative)
-     
-     
+     elNative.src = this.customImg
   }
+ 
+
   
   constructor(private elHost: ElementRef) {
    }
